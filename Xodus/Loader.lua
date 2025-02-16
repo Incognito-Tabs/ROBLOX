0bcf11179b7c866249cd0492044c5c5b33ae9598
Incognito <incognito.private.use@gmail.com>
2025-02-16 04:33:35 +0300

    Update Loader.lua

--- Xodus/Loader.lua
+++ Xodus/Loader.lua
@@ -1,1 +1,11 @@
+local List = {
+    [4282985734] = "https://raw.githubusercontent.com/Incognito-RBX/Roblox/refs/heads/main/Xodus/Blackout%Warriors.lua",
+    [4588604953] = "https://raw.githubusercontent.com/Incognito-RBX/Roblox/refs/heads/main/Xodus/Criminality.lua",
+    [8767500166] = "https://raw.githubusercontent.com/Incognito-RBX/Roblox/refs/heads/main/Xodus/Blackout.lua",
+}
 
+if List[game.GameId] then
+    return loadstring(game:HttpGet(List[game.GameId]))()
+else
+    return game.Players.LocalPlayer:Kick("Game Not Supported.")
+end
