72d1e049d09c45925877527aadf0981da47e7c97
Incognito <incognito.private.use@gmail.com>
2025-02-16 04:38:52 +0300

    Update Loader.lua

--- Xodus/Loader.lua
+++ Xodus/Loader.lua
@@ -1,7 +1,7 @@
 local List = {
-    [4282985734] = "https://raw.githubusercontent.com/Incognito-RBX/Roblox/refs/heads/main/Xodus/Blackout%Warriors.lua",
-    [4588604953] = "https://raw.githubusercontent.com/Incognito-RBX/Roblox/refs/heads/main/Xodus/Criminality.lua",
-    [8767500166] = "https://raw.githubusercontent.com/Incognito-RBX/Roblox/refs/heads/main/Xodus/Blackout.lua",
+    [1390601379] = "https://raw.githubusercontent.com/Incognito-RBX/Roblox/refs/heads/main/Xodus/Blackout%Warriors.lua",
+    [1494262959] = "https://raw.githubusercontent.com/Incognito-RBX/Roblox/refs/heads/main/Xodus/Criminality.lua",
+    [3326279937] = "https://raw.githubusercontent.com/Incognito-RBX/Roblox/refs/heads/main/Xodus/Blackout.lua",
 }
 
 if List[game.GameId] then
